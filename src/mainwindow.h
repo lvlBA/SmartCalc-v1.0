b2a225bad234b4a9f32b4da25e04e740c5c4d2b6
Bernardina Program <bernardi@un-l4.kzn.21-school.ru>
2023-01-10 15:49:32 +0300

    develop

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -8,26 +8,27 @@ extern "C" {
 }
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+ public:
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+ private:
+  Ui::MainWindow *ui;
 
-private slots:
-    void digits_numbers();
-    void on_pushButton_dot_clicked();
-    void operations();
-    void on_pushButton_equal_clicked();
-    void on_pushButton_DELETE_clicked();
-    void on_pushButton_X_clicked();
+ private slots:
+  void digits_numbers();
+  void on_pushButton_dot_clicked();
+  void operations();
+  void on_pushButton_equal_clicked();
+  void on_pushButton_DELETE_clicked();
+  void on_pushButton_X_clicked();
 };
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
